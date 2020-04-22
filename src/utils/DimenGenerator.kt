8445e35adf81387eba341a3cab2752602c88e819
qiugang <1607868475@qq.com>
2020-04-22 11:59:50 +0800

    '修改代码风格'

--- src/utils/DimenGenerator.kt
+++ src/utils/DimenGenerator.kt
@@ -17,46 +17,46 @@ private val dimenRange = -10..1280
 private val fileDimenSizeArr = arrayOf(240, 270, 360, 384, 400, 411)
 
 fun main() {
-    fileDimenSizeArr.forEach {
-        val saveDir = File("./result")
-        if (saveDir.exists()) {
-            saveDir.delete()
-        }
-        saveDir.mkdirs()
-        val dimenDir = File(saveDir, "values-w${it}dp")
-        if (dimenDir.exists()) {
-            dimenDir.delete()
-        }
-        dimenDir.mkdirs()
-        val dimenFile = File(dimenDir, "dimens.xml")
-        val res = genDimenFile(it, 360)
-        println("generate dimen --> w${it}dp")
-        dimenFile.writeText(res)
+  fileDimenSizeArr.forEach {
+    val saveDir = File("./result")
+    if (saveDir.exists()) {
+      saveDir.delete()
     }
+    saveDir.mkdirs()
+    val dimenDir = File(saveDir, "values-w${it}dp")
+    if (dimenDir.exists()) {
+      dimenDir.delete()
+    }
+    dimenDir.mkdirs()
+    val dimenFile = File(dimenDir, "dimens.xml")
+    val res = genDimenFile(it, 360)
+    println("generate dimen --> w${it}dp")
+    dimenFile.writeText(res)
+  }
 }
 
 fun genDimenFile(currentSize: Int, baseSize: Int): String {
-    val dimenBuilder = StringBuilder()
-    dimenBuilder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
-    dimenBuilder.append(System.lineSeparator())
-    dimenBuilder.append("<resources>")
+  val dimenBuilder = StringBuilder()
+  dimenBuilder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+  dimenBuilder.append(System.lineSeparator())
+  dimenBuilder.append("<resources>")
 
-    dimenRange.forEach {
-        if (it != 0) {
-            dimenBuilder.append(System.lineSeparator())
-            dimenBuilder.append("\t")
-            val pxUnit = if (it < 0) {
-                "px_${abs(it)}"
-            } else {
-                "px${it}"
-            }
-            val pxValue = "${decimalFormat.format(
-                it * dimenScale * (currentSize.toFloat() / baseSize.toFloat())
-            )}dp"
-            dimenBuilder.append("<dimen name=\"${pxUnit}\">${pxValue}</dimen>")
-        }
+  dimenRange.forEach {
+    if (it != 0) {
+      dimenBuilder.append(System.lineSeparator())
+      dimenBuilder.append("\t")
+      val pxUnit = if (it < 0) {
+        "px_${abs(it)}"
+      } else {
+        "px${it}"
+      }
+      val pxValue = "${decimalFormat.format(
+          it * dimenScale * (currentSize.toFloat() / baseSize.toFloat())
+      )}dp"
+      dimenBuilder.append("<dimen name=\"${pxUnit}\">${pxValue}</dimen>")
     }
-    dimenBuilder.append(System.lineSeparator())
-    dimenBuilder.append("</resources>")
-    return dimenBuilder.toString()
+  }
+  dimenBuilder.append(System.lineSeparator())
+  dimenBuilder.append("</resources>")
+  return dimenBuilder.toString()
 }
